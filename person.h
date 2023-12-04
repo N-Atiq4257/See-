21d601e0d0793120f64c73604751af246273adda
Atiq, Nafil <111777614+N-Atiq4257@users.noreply.github.com>
2023-12-04 15:13:27 -0600

    awawawa

--- person.h
+++ person.h
@@ -26,10 +26,16 @@ class Person{
       age = a;
     }
 
+    // Destructor
+    ~Person(){}
+
+    // Get functions
     int getAge(){ return age; }
+    // Void since strings/arrays aren't returned.
     void getlname(char* temp){ strcpy(temp, lname); }
     void getfname(char* temp){ strcpy(temp, fname); }
 
+    // Srt functions
     int setAge(int a){
       // do some validation, yada yada
       age = a;
@@ -40,8 +46,40 @@ class Person{
     void setfname(char* f){
       strcpy(fname, f);
     }
+
+    // Tostring!! i forgot this.
+    void toString(char* temp){
+      strcpy(temp, lname);
+      strcat(temp, ", ");
+      strcat(temp, fname);
+      // :)
+    }
 };
+// outside of class, this is where operator overloading happens.
 
+// overloading for ==
+int operator==(Person x, Person y){
+  // This is one way to do so.
+  return x.getAge() == y.getAge();
+  /**
+  Alternate Method!!!!
+  
+  char temp[41], temp2[41];
+  x.toString(temp);
+  y.toString(temp2);
 
+  return strcmp(temp, temp2) == 0;
+  **/
+}
+
+// IOSTREAM operators!!! they're (sorta) special!!!!
+ostream& operator<<(ostream& os, Person x){
+  char temp[41];
+  x.toString(temp);
+  
+  os << temp << ", age " << x.getAge();
+
+  return os;
+}
 
 #endif
